6fe3e1cef2fb0588da2cc172a22e977be26288c8
Otavio Cals <otaviolcals@gmail.com>
2016-09-07 22:41:13 -0300

    added plotter

--- lens_matrix.R
+++ lens_matrix.R
@@ -5,7 +5,8 @@ lens_matrix <- function(lens_table)
 	refract_matrices <- list()
 	trans_matrices <- list()
 
-	final_matrix <- matrix(c(1,0,0,1),nrow=2,ncol=2, byrow=TRUE)
+	final_matrix <- list()
+	final_matrix[[1]] <- matrix(c(1,0,0,1),nrow=2,ncol=2, byrow=TRUE)
 
 	for(i in 1:refract_num)
 	{
@@ -16,12 +17,12 @@ lens_matrix <- function(lens_table)
 			d <- -1*(lens_table[i+1,3] - lens_table[i,3])
 			trans_matrices[[i]]<-matrix(c(1,0,d,1),nrow=2,ncol=2, byrow=TRUE)
 
-			final_matrix <- refract_matrices[[i]] %*% final_matrix
-			final_matrix <- trans_matrices[[i]] %*% final_matrix
+			final_matrix[[i]] <- refract_matrices[[i]] %*% final_matrix[[i]]
+			final_matrix[[i+1]] <- trans_matrices[[i]] %*% final_matrix[[i]]
 		}
 		else
 		{
-			final_matrix <- refract_matrices[[i]] %*% final_matrix
+			final_matrix[[i]] <- refract_matrices[[i]] %*% final_matrix[[i]]
 		}
 	}
 	final_matrix
--- telescope_schematics.R
+++ telescope_schematics.R
@@ -1,14 +1,31 @@
 telescope_schematics <- function(lens_table,obj)
 {
+	library("ggplot2")
 	source("lens_matrix.R")
 
+	
 	lens_table <- lens_table[order(lens_table[,3]),]
+	telescope_matrix <- list()
+	image_matrix <- list()
+	images <- list()
+	inter_image_pos <- list()
+
 
 	obj_matrix <- matrix(c(1/obj,1),nrow=2,ncol=1,byrow=TRUE)
-	telescope_matrix <- lens_matrix(lens_table)
-	image_matrix <- telescope_matrix %*% obj_matrix
-	image <- image_matrix[1,1]/image_matrix[2,1]
-	image <- 1/image
+	matrix_data <- lens_matrix(lens_table)
+	for(i in 1:length(matrix_data))
+	{
+		telescope_matrix[[i]] <- matrix_data[[i]]
+		image_matrix[[i]] <- telescope_matrix[[i]] %*% obj_matrix
+		images[[i]] <- image_matrix[[i]][1,1]/image_matrix[[i]][2,1]
+		images[[i]] <- 1/images[[i]]
+		image<-images[[i]]
+	}
+#	telescope_matrix <- matrix_data[[length(matrix_data)]]
+#	image_matrix <- telescope_matrix %*% obj_matrix
+#	image <- image_matrix[1,1]/image_matrix[2,1]
+#	image <- 1/image
+#	print(images)
 
 	telescope_length <- lens_table[nrow(lens_table),3] - lens_table[1,3]
 
@@ -18,27 +35,204 @@ telescope_schematics <- function(lens_table,obj)
 	if(obj>2*telescope_length)
 	{
 		start_pos <- lens_table[1,3]-(telescope_length/10)
+		infinite_start <- TRUE
 	}
 	else
 	{
 		start_pos <- lens_table[1,3]-obj
+		infinite_start <- FALSE
 	}
 
 	print("Starting Position:")
 	print(start_pos)
 
-	if(image>2*telescope_length)
+	if(image>lens_table[nrow(lens_table),2]*3)
 	{
 		end_pos <- lens_table[nrow(lens_table),3]+(telescope_length/10)
+		infinite_end <- TRUE
 	}
 	else
 	{
 		end_pos <- lens_table[nrow(lens_table),3]+image
+		infinite_end <- FALSE
 	}
 
 	print("Ending Position:")
 	print(end_pos)
 
 	print("Image Distance:")
-	image
+	print(image)
+
+	plot_height <- 3*max(lens_table[,4])
+	tel_height <- max(lens_table[,4])*0.5
+
+	plot_graph <- ggplot() + geom_point() + xlim(start_pos, end_pos) + ylim(-plot_height, plot_height)
+
+	previous_lens_position <- lens_table[1,3]
+	previous_lens_width <- lens_table[1,4]*0.15
+	previous_ray_end <- lens_table[1,4]*0.5
+
+
+	for(i in 1:nrow(lens_table))
+	{
+		lens_position <- lens_table[i,3]
+		lens_height <- lens_table[i,4]*0.5
+		lens_width <- lens_height*0.3
+
+		lens_distance <- lens_position - previous_lens_position
+		width_sum <- lens_width + previous_lens_width
+
+
+#Position Correction
+		if(lens_distance>0 && lens_distance < width_sum)
+		{
+			lens_position <- previous_lens_position + width_sum
+			plot_graph <- plot_graph + annotate("segment",x=previous_lens_position,xend=lens_position,y=previous_ray_end,yend=previous_ray_end, color="dodgerblue")
+			plot_graph <- plot_graph + annotate("segment",x=previous_lens_position,xend=lens_position,y=-1*previous_ray_end,yend=-1*previous_ray_end, color="dodgerblue")
+		}
+
+#Convex Lens
+		if(lens_table[i,2]>0)
+		{
+			plot_graph <- plot_graph + annotate("path",
+ 			  x=lens_position + lens_width*cos(seq(0,2*pi,length.out=100)),
+			   y= 0 + lens_height*sin(seq(0,2*pi,length.out=100)))
+		}
+
+#Concave Lens
+		else if (lens_table[i,2]<0)
+		{
+			plot_graph <- plot_graph + annotate("path",
+ 			  x=lens_position + lens_width*cos(seq(-1/2*pi,1/2*pi,length.out=100))-2*lens_width,
+			   y= 0 + lens_height*sin(seq(-1/2*pi,1/2*pi,length.out=100)))
+
+			plot_graph <- plot_graph + annotate("path",
+ 			  x=lens_position + lens_width*cos(seq(1/2*pi,3/2*pi,length.out=100))+2*lens_width,
+			   y= 0 + lens_height*sin(seq(1/2*pi,3/2*pi,length.out=100)))
+
+			plot_graph <- plot_graph + annotate("segment",x=lens_position-2*lens_width,xend=lens_position+2*lens_width,y=lens_height,yend=lens_height)
+			plot_graph <- plot_graph + annotate("segment",x=lens_position-2*lens_width,xend=lens_position+2*lens_width,y=-lens_height,yend=-lens_height)
+
+		}
+
+#Name Plotter
+		if(i%%2!=0)
+		{
+			plot_graph <- plot_graph + annotate("text",x=lens_position,y=plot_height*0.25 + (plot_height*0.05*(floor(i/2))%%2),label=lens_table[i,1])
+		}
+		else
+		{
+			plot_graph <- plot_graph + annotate("text",x=lens_position,y=-1*plot_height*0.25 - (plot_height*0.05*(floor(i/2))%%2),label=lens_table[i,1])
+		}
+
+#Rays Tracer
+
+		inter_image_pos[[i]] <- lens_position + images[[i]]
+		print(inter_image_pos[[i]])
+	#Real Inverted Image
+		if (i < nrow(lens_table) && inter_image_pos[[i]] > lens_table[i,3] && inter_image_pos[[i]] < lens_table[i+1,3] && inter_image_pos[[i]] > start_pos && inter_image_pos[[i]] < end_pos)
+		{
+			plot_graph <- plot_graph + annotate("point",x=inter_image_pos[[i]],y=0)
+			slope <- -1*previous_ray_end/(inter_image_pos[[i]]-lens_position)
+			ray_end <- slope*(lens_table[i+1,3]-lens_position) + previous_ray_end
+			if(abs(ray_end) < tel_height)
+			{
+				plot_graph <- plot_graph + annotate("segment",x=lens_position,xend=lens_table[i+1,3],y=previous_ray_end,yend=ray_end, color="dodgerblue")
+				plot_graph <- plot_graph + annotate("segment",x=lens_position,xend=lens_table[i+1,3],y=-1*previous_ray_end,yend=-1*ray_end, color="dodgerblue")
+				previous_ray_end <- ray_end
+			}
+			else
+			{
+				mid_point <- ((-1*tel_height)-previous_ray_end)/slope + lens_position
+				plot_graph <- plot_graph + annotate("segment",x=lens_position,xend=mid_point,y=previous_ray_end,yend=-1*tel_height, color="dodgerblue")
+				plot_graph <- plot_graph + annotate("segment",x=lens_position,xend=mid_point,y=-1*previous_ray_end,yend=tel_height, color="dodgerblue")
+				previous_ray_end <- tel_height
+				plot_graph <- plot_graph + annotate("segment",x=mid_point,xend=lens_table[i+1,3],y=previous_ray_end,yend=lens_table[i+1,4]*0.5, color="dodgerblue")
+				plot_graph <- plot_graph + annotate("segment",x=mid_point,xend=lens_table[i+1,3],y=-1*previous_ray_end,yend=-1*lens_table[i+1,4]*0.5, color="dodgerblue")
+			}
+
+		}
+	#Real Not Inverted Image
+		else if(i < nrow(lens_table) && inter_image_pos[[i]] > lens_table[i,3] && inter_image_pos[[i]] > lens_table[i+1,3] && inter_image_pos[[i]] > start_pos && inter_image_pos[[i]] < end_pos)
+		{
+			slope <- -1*previous_ray_end/(inter_image_pos[[i]]-lens_position)
+			ray_end <- slope*(lens_table[i+1,3]-lens_position) + previous_ray_end
+			if(ray_end < tel_height)
+			{
+				plot_graph <- plot_graph + annotate("segment",x=lens_position,xend=lens_table[i+1,3],y=previous_ray_end,yend=ray_end, color="dodgerblue")
+				plot_graph <- plot_graph + annotate("segment",x=lens_position,xend=lens_table[i+1,3],y=-1*previous_ray_end,yend=-1*ray_end, color="dodgerblue")
+				previous_ray_end <- ray_end
+			}
+			else
+			{
+				mid_point <- ((-1*tel_height)-previous_ray_end)/slope + lens_position
+				plot_graph <- plot_graph + annotate("segment",x=lens_position,xend=mid_point,y=previous_ray_end,yend=-1*tel_height, color="dodgerblue")
+				plot_graph <- plot_graph + annotate("segment",x=lens_position,xend=mid_point,y=-1*previous_ray_end,yend=tel_height, color="dodgerblue")
+				previous_ray_end <- tel_height
+				plot_graph <- plot_graph + annotate("segment",x=mid_point,xend=lens_table[i+1,3],y=previous_ray_end,yend=lens_table[i+1,4]*0.5, color="dodgerblue")
+				plot_graph <- plot_graph + annotate("segment",x=mid_point,xend=lens_table[i+1,3],y=-1*previous_ray_end,yend=-1*lens_table[i+1,4]*0.5, color="dodgerblue")
+			}
+			
+		}
+	#Virtual Image (STILL NEEDS TESTING!!!!!!)
+		else if(i < nrow(lens_table) && inter_image_pos[[i]] < lens_table[i,3] && inter_image_pos[[i]] > start_pos && inter_image_pos[[i]] < end_pos)
+		{
+			slope <- -1*previous_ray_end/(inter_image_pos[[i]]-lens_position)
+			ray_end <- slope*(lens_table[i+1,3]-lens_position) + previous_ray_end
+			if(ray_end < tel_height)
+			{
+				plot_graph <- plot_graph + annotate("segment",x=lens_position,xend=lens_table[i+1,3],y=previous_ray_end,yend=ray_end, color="dodgerblue")
+				plot_graph <- plot_graph + annotate("segment",x=lens_position,xend=lens_table[i+1,3],y=-1*previous_ray_end,yend=-1*ray_end, color="dodgerblue")
+				previous_ray_end <- ray_end
+			}
+			else
+			{
+				mid_point <- ((-1*tel_height)-previous_ray_end)/slope + lens_position
+				plot_graph <- plot_graph + annotate("segment",x=lens_position,xend=mid_point,y=previous_ray_end,yend=-1*tel_height, color="dodgerblue")
+				plot_graph <- plot_graph + annotate("segment",x=lens_position,xend=mid_point,y=-1*previous_ray_end,yend=tel_height, color="dodgerblue")
+				previous_ray_end <- tel_height
+				plot_graph <- plot_graph + annotate("segment",x=mid_point,xend=lens_table[i+1,3],y=previous_ray_end,yend=lens_table[i+1,4]*0.5, color="dodgerblue")
+				plot_graph <- plot_graph + annotate("segment",x=mid_point,xend=lens_table[i+1,3],y=-1*previous_ray_end,yend=-1*lens_table[i+1,4]*0.5, color="dodgerblue")
+			}
+		}
+	#Image on Infinity
+		else if(i < nrow(lens_table) && (inter_image_pos[[i]] < start_pos || inter_image_pos[[i]] > end_pos))
+		{
+			plot_graph <- plot_graph + annotate("segment",x=lens_position,xend=lens_table[i+1,3],y=previous_ray_end,yend=previous_ray_end, color="dodgerblue")
+			plot_graph <- plot_graph + annotate("segment",x=lens_position,xend=lens_table[i+1,3],y=-1*previous_ray_end,yend=-1*previous_ray_end, color="dodgerblue")
+		}
+
+
+		previous_lens_position <- lens_position
+		previous_lens_width <- lens_width
+
+	}
+	
+#Object Plotter
+
+	if(infinite_start)
+	{
+		plot_graph <- plot_graph + annotate("segment",x=start_pos,xend=lens_table[1,3],y=lens_table[1,4]*0.5,yend=lens_table[1,4]*0.5, color="dodgerblue")
+		plot_graph <- plot_graph + annotate("segment",x=start_pos,xend=lens_table[1,3],y=-1*lens_table[1,4]*0.5,yend=-1*lens_table[1,4]*0.5, color="dodgerblue")
+	}
+	else
+	{
+		plot_graph <- plot_graph + annotate("segment",x=start_pos,xend=lens_table[1,3],y=0,yend=lens_table[1,4]*0.5, color="dodgerblue")
+		plot_graph <- plot_graph + annotate("segment",x=start_pos,xend=lens_table[1,3],y=0,yend=-1*lens_table[1,4]*0.5, color="dodgerblue")
+	}
+
+#Image Plotter
+
+	if(infinite_end)
+	{
+		plot_graph <- plot_graph + annotate("segment",x=lens_table[nrow(lens_table),3],xend=end_pos,y=previous_ray_end,yend=previous_ray_end, color="dodgerblue")
+		plot_graph <- plot_graph + annotate("segment",x=lens_table[nrow(lens_table),3],xend=end_pos,y=-1*previous_ray_end,yend=-1*previous_ray_end, color="dodgerblue")
+	}
+	else
+	{
+		plot_graph <- plot_graph + annotate("segment",x=lens_table[nrow(lens_table),3],xend=end_pos,y=previous_ray_end,yend=0, color="dodgerblue")
+		plot_graph <- plot_graph + annotate("segment",x=lens_table[nrow(lens_table),3],xend=end_pos,y=-1*previous_ray_end,yend=0, color="dodgerblue")	
+	}
+
+	plot_graph
 }
